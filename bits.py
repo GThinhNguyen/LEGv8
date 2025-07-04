a0c0db0a0bf850b0837a79494b4f65bd1a952bf5
Thinh <nguyengiathinh595@gmail.com>
2025-07-04 10:20:55 +0700

    fix znvc

--- bits.py
+++ bits.py
@@ -451,7 +451,7 @@ def get_bits_for_path(block, ui = None):
         
         nzcv = data['Flags']['NZCV']  # string 4 ký tự
         n, z, c, v = nzcv[0], nzcv[1], nzcv[2], nzcv[3]
-
+        n, z, c, v = int(n), int(z), int(c), int(v) 
         def highlight_flag(label, bit):
             if bit == '1':
                 label.setStyleSheet(label.styleSheet() + "background-color: lightgreen;")
